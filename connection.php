8562076021a691aa38aa3141910f0c9d1eda97a4
BishnoiYash <54094583+BishnoiYash@users.noreply.github.com>
2020-05-24 17:54:27 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
 $host="ec2-50-17-90-177.compute-1.amazonaws.com";
 $user="wrxdynhwnymdiy";
 $password="78317618e3b727c8101e506d53ee0e3402f84e421c69d6849d312f60c4714172";
-$Database="d223i1n3urqin6";
+$dbbase="d223i1n3urqin6";
 $port="5432";
 
-$cont=mysqli_connect($host,$user,$password,$Database);
+$cont=mysqli_connect($host,$user,$password,$dbbase);
 if(isset($cont))
 {
 
