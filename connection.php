2384c379cdbd981d7fe7327e8acf02ac80f1645c
BishnoiYash <54094583+BishnoiYash@users.noreply.github.com>
2020-05-24 17:53:08 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
-$host="localhost";
-$user="root";
-$pass="";
-$db="pagess";
+$host="ec2-50-17-90-177.compute-1.amazonaws.com";
+$user="wrxdynhwnymdiy";
+$password="78317618e3b727c8101e506d53ee0e3402f84e421c69d6849d312f60c4714172";
+$Database="d223i1n3urqin6";
+$port="5432";
 
-
-$cont=mysqli_connect($host,$user,$pass,$db);
+$cont=mysqli_connect($host,$user,$password,$Database);
 if(isset($cont))
 {
 
